e351d36da26da74cae3d41017c0e6507c2f95568
Дмитрий Пархейчук <shikofx@gmail.com>
2025-11-06 14:10:27 +0300

    Espresso: Set up infra and add first test

--- app/src/androidTest/java/by/pda/demoapp/ui/espresso/AboutFragmentTest.kt
+++ app/src/androidTest/java/by/pda/demoapp/ui/espresso/AboutFragmentTest.kt
@@ -4,17 +4,38 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import by.pda.demoapp.android.R
 import by.pda.demoapp.android.view.fragments.AboutFragment
 import by.pda.demoapp.ui.common.test.FragmentTest
+import io.qameta.allure.Epic
+import io.qameta.allure.Feature
+import io.qameta.allure.Story
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
-class AboutFragmentTest: FragmentTest<AboutFragment>(AboutFragment::class.java) {
+@Epic("UI-Testing (Espresso)")
+@Feature("About screen")
+@Tag("espresso")
+@Tag("component-ui")
+class AboutFragmentTest : FragmentTest<AboutFragment>(AboutFragment::class.java) {
 
+    @Story("AF-STORY-1: Display basic information about product")
     @Test
-    fun elementsAreDisplayed() {
+    fun applicationVersionIsDisplayed() {
         onView(withId(R.id.versionTV))
             .check(matches(isDisplayed()))
+            .check(matches(withText(R.string.app_version)))
     }
+
+    @Story("AF-STORY-1: Display basic information about product")
+    @Test
+    fun webLinkIsDisplayed() {
+        onView(withId(R.id.webTV))
+            .check(matches(isDisplayed()))
+            .check(matches(withText(R.string.go_to_website)))
+    }
+
+
 
 }
